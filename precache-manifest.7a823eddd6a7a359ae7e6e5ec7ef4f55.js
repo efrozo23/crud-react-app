8319b97e570b1eb0899cb9436c6276ef87d95a68
Elkin <relkinf1@gmail.com>
2020-07-02 18:32:15 -0500

    Updates

---
+++ precache-manifest.7a823eddd6a7a359ae7e6e5ec7ef4f55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1f8a436cf050ae5459e4032ed018ee",
+    "url": "/crud-react-app/index.html"
+  },
+  {
+    "revision": "271769b4cb3c19df19ab",
+    "url": "/crud-react-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "52a2bf98907a1dcfc98e",
+    "url": "/crud-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "271769b4cb3c19df19ab",
+    "url": "/crud-react-app/static/js/2.b14cf026.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/crud-react-app/static/js/2.b14cf026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52a2bf98907a1dcfc98e",
+    "url": "/crud-react-app/static/js/main.8922f42c.chunk.js"
+  },
+  {
+    "revision": "23411efd3f8364690bf2",
+    "url": "/crud-react-app/static/js/runtime-main.de1677a0.js"
+  }
+]);
